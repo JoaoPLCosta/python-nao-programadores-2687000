f2b2c63eea292386d1583fd355dc79a13d8bba6b
João Pedro Leite Costa <95060373+JoaoPLCosta@users.noreply.github.com>
2024-05-13 17:14:30 +0000

    resolucao exercicio 3

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,13 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+#print(type(data_nascimento)) - string (texto)
+#print(type(idade_numerica)) - integer (inteiro)
+#print(type(altura)) - float (decimal)
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+#print(f"Pessoas que nasceram em {data_nascimento} podem ter mais ou menos de {idade_numerica} anos de idade.")
 
 # Realize uma operação entre dados do tipo inteiro e float
+#soma = idade_numerica + altura
+#print(soma)
